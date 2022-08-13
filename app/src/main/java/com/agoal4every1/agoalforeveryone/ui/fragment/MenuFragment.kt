4bdf22a0220dca2d8d30bf1d89c30629134c82b7
BogibekDev <bogibekdev@gmail.com>
2022-08-13 11:02:35 +0000

    button and navigate added

--- app/src/main/java/com/agoal4every1/agoalforeveryone/ui/fragment/MenuFragment.kt
+++ app/src/main/java/com/agoal4every1/agoalforeveryone/ui/fragment/MenuFragment.kt
@@ -3,6 +3,7 @@ package com.agoal4every1.agoalforeveryone.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.agoal4every1.agoalforeveryone.R
 import com.agoal4every1.agoalforeveryone.databinding.FragmentMenuBinding
 import com.agoal4every1.agoalforeveryone.utils.viewBinding
@@ -13,6 +14,20 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
     private val binding by viewBinding { FragmentMenuBinding.bind(it) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding.apply {
+
+            bSetting.setOnClickListener {
+                findNavController().navigate(R.id.action_menuFragment_to_settingFragment)
+            }
+
+            bGame.setOnClickListener {
+                findNavController().navigate(R.id.action_menuFragment_to_gameFragment)
+            }
+        }
     }
 
 }
